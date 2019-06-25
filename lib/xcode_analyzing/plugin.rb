30816090ace293bfd9add0f84126eb404a13189c
WataruSuzuki <wataru0406@gmail.com>
2019-06-25 22:30:00 +0900

    hoge fuga

--- lib/xcode_analyzing/plugin.rb
+++ lib/xcode_analyzing/plugin.rb
@@ -1,33 +1,68 @@
 module Danger
-  # This is your plugin class. Any attributes or methods you expose here will
-  # be available from within your Dangerfile.
-  #
-  # To be published on the Danger plugins site, you will need to have
-  # the public interface documented. Danger uses [YARD](http://yardoc.org/)
-  # for generating documentation from your plugin source, and you can verify
-  # by running `danger plugins lint` or `bundle exec rake spec`.
-  #
-  # You should replace these comments with a public description of your library.
-  #
-  # @example Ensure people are well warned about merging on Mondays
-  #
-  #          my_plugin.warn_on_mondays
-  #
-  # @see  WataruSuzuki/danger-xcode_analyzing
-  # @tags monday, weekends, time, rattata
-  #
-  class DangerXcodeAnalyzing < Plugin
+    class DangerXcodeAnalyzing < Plugin
+        require 'Fileutils'
+        require 'rexml/document'
 
-    # An attribute that you can read/write from your Dangerfile
-    #
-    # @return   [Array<String>]
-    attr_accessor :my_attribute
+        attr_accessor :diff_files
+        def diff_files
+            return diff_files = (git.modified_files - git.deleted_files) + git.added_files
+        end
 
-    # A method that you can call from your Dangerfile
-    # @return   [Array<String>]
-    #
-    def warn_on_mondays
-      warn 'Trying to merge code on a Monday' if Date.today.wday == 1
+        attr_accessor :xcodebuild_project
+        attr_accessor :xcodebuild_scheme
+        attr_accessor :xcodebuild_configuration
+
+        attr_accessor :analyzedResultsDir
+        def analyzedResultsDir
+            return 'clang/' + @analyzedResultsDir
+        end
+
+        def report
+            if xcodebuild_project.empty? && xcodebuild_scheme.empty?
+                warn("(- -;;) dangerプラグインのエラー", sticky: false)
+                return
+            end
+
+            system "xcodebuild analyze -project #{xcodebuild_project} -scheme #{xcodebuild_scheme} -configuration #{xcodebuild_configuration} CLANG_ANALYZER_OUTPUT=plist CLANG_ANALYZER_OUTPUT_DIR=\"$(pwd)/clang\""
+
+            unless FileTest.exists? analyzedResultsDir
+                fail("(・A・)!! #{analyzedResultsDir}が見当たりません、ビルドに失敗しているか無効なディレクトリ指定です", sticky: false)
+                return
+            end
+
+            Dir.foreach(analyzedResultsDir) do |file|
+                puts file
+                if file.end_with?(".plist")
+                    targetFileName = nil
+                    doc = REXML::Document.new(File.new(analyzedResultsDir + "/" + file))
+                    doc.elements.each("plist/dict/array") do |element|
+                        element.elements.each("string") do |filename|
+                            diff_files.each do |target|
+                                if filename.text.include? target
+                                    targetFileName = target
+                                end
+                            end
+                        end
+                        unless targetFileName == nil
+                            element.elements.each("dict/array/dict/dict") do |child|
+                                if child.elements['key'].text == 'line'
+                                    element.elements.each("dict/array/dict") do |messages|
+                                        messages.elements.each_with_index() do |key, index|
+                                            if key.text == "message"
+                                                offset = (index + 2) # <- 1始まり、かつ次の要素
+                                                message = messages.elements[offset].text
+                                                unless message.empty?
+                                                    warn(message, file: targetFileName, line: child.elements['integer'].text.to_i)
+                                                end
+                                            end
+                                        end
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
     end
-  end
 end
